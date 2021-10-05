94c373e914159398343f22b22d7e9a201fd4fba8
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-05 21:52:22 +0200

    create database

--- main/models.py
+++ main/models.py
@@ -10,3 +10,9 @@ class ToDoList(models.Model):
 
 class Item(models.Model):
     todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.text
+
