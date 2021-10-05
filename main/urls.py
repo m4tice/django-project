5e9245a996ea118e90275bcf3caec4f6e2932c25
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-06 00:37:55 +0200

    Templates and Custom HTML

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     # path("", views.index, name="index"),
-    path("<str:name>", views.index, name="index")
+    path("<int:id>", views.index, name="index"),
+    path("", views.home, name="home")
 ]
