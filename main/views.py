745f0d4d29b27d89e56de51163deedd6e33ae844
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-06 16:17:04 +0200

    Custom Forms

--- main/views.py
+++ main/views.py
@@ -10,6 +10,25 @@ def index(response, id):
     ls = ToDoList.objects.get(id=id)
     # item = ls.item_set.get(id=1)
     # return HttpResponse('<h1>%s</h1><br><p>%s</p>' % (ls.name, str(item.text)))
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False    
+
+                item.save()
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+            if len(txt) > 3:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("Invalid")
+
+
     return render(response, "main/list.html", {"ls": ls})
 
 
