da1e0c7fd4ae8bf40aaad14b5f73fb0ffbab2d2b
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-06 14:26:03 +0200

    Simple Forms

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
+from .forms import CreateNewList
 
 
 # Create your views here.
@@ -13,3 +15,20 @@ def index(response, id):
 
 def home(response):
     return render(response, "main/home.html", {})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            t = ToDoList(name=name)
+            t.save()
+
+        return HttpResponseRedirect("/%i" % t.id)
+
+    else:
+        form = CreateNewList()
+
+    return render(response, "main/create.html", {"form": form})
