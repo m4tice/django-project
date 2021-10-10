c5259f7460722ca04f90136dc53535b561802ade
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-10 16:44:09 +0200

    User Authentication

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # path("", views.index, name="index"),
     path("<int:id>", views.index, name="index"),
     path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("create/", views.create, name="create")
 ]
