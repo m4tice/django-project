8350189dd0982d352951f7fb2e66d9737aadfdf1
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-10 16:17:26 +0200

    User registration and Sign Up page

--- m4ticesite/settings.py
+++ m4ticesite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig'
+    'crispy_forms',
+    'main.apps.MainConfig',
+    'register.apps.RegisterConfig'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
